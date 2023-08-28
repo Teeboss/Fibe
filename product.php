2105b77b765b2fc67a8f4a4aed5b6acc2a1976c3
Tobiloba Habib <65220027+Teeboss@users.noreply.github.com>
2023-08-28 15:07:11 +0100

    Update product.php

--- product.php
+++ product.php
@@ -142,7 +142,7 @@ paymentForm.addEventListener("submit", payWithPaystack, false);
 function payWithPaystack(e) {
   e.preventDefault();
   let handler = PaystackPop.setup({
-    key: 'pk_live_f8d56c629b844896e3d1f8bac8b02541799751ea', // Replace with your public key
+    key: 'Your-public-key', // Replace with your public key
     email: document.getElementById("email-address").value,
     amount: document.getElementById("amount").value * 100,
     ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
